f0513ed5bb2365d1df7aaf4203f785eba6d13a2a
lusijanka <lucjamiodek@gmail.com>
2017-07-30 14:38:56 +0200

    script modified

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// Create a variable name 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
+/* Display the text in the console
+Not visible
+*/
 console.log('Hello, ' + name);
